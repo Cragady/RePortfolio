db3f7ce2e7c1ee5fde178492a4068ff97b63f4e0
Cragady <pinkdotdreamer@gmail.com>
2018-12-13 14:40:38 -0700

    cleaning seeds

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -2,32 +2,10 @@ const mongoose = require('mongoose'),
     db = require('../models'),
     portData = require('./portData.json');
 
-// console.log(portData[0].link);
-
 mongoose.connect(
     process.env.MONGODB_URI ||
     'mongodb://localhost/portyPorts'
 );
-
-// db.PicData.remove({})
-//     .then(() =>{//start chain
-
-// portData.forEach(function(item, iterator){
-//     db.PicData  
-//         .updateOne({'title': item.title}, item, {upsert: true, '$setOnInsert': {'title': item.title}})
-//         .then(data => {
-//             console.log(data + ' records inserted!');
-//             if(iterator === portData.length - 1){
-//                 process.exit(0);
-//             };
-//         })
-//         .catch(err => {
-//             console.error(err);
-//             process.exit(1);
-//         });
-// });
-
-// }); //end chain
 
 function seeder(iterator, cb){
     let item = portData[iterator];
