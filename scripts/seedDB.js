69504c72f80251c6677836b23f7427a23c4f4049
Cragady <pinkdotdreamer@gmail.com>
2018-12-13 14:40:02 -0700

    added more pictures and more to portfolio page; used a recursive method instead of a loop to seed database, is faster

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -9,23 +9,72 @@ mongoose.connect(
     'mongodb://localhost/portyPorts'
 );
 
-db.PicData.remove({})
-    .then(() =>{//start chain
+// db.PicData.remove({})
+//     .then(() =>{//start chain
 
-portData.forEach(function(item, iterator){
+// portData.forEach(function(item, iterator){
+//     db.PicData  
+//         .updateOne({'title': item.title}, item, {upsert: true, '$setOnInsert': {'title': item.title}})
+//         .then(data => {
+//             console.log(data + ' records inserted!');
+//             if(iterator === portData.length - 1){
+//                 process.exit(0);
+//             };
+//         })
+//         .catch(err => {
+//             console.error(err);
+//             process.exit(1);
+//         });
+// });
+
+// }); //end chain
+
+function seeder(iterator, cb){
+    let item = portData[iterator];
     db.PicData  
-        .updateOne({'title': item.title}, item, {upsert: true, '$setOnInsert': {'title': item.title}})
-        .then(data => {
-            console.log(data + ' records inserted!');
-            if(iterator === portData.length - 1){
+        .updateOne(
+            {'title': item.title}, 
+            item, 
+            {
+                upsert: true, 
+                '$setOnInsert': {'title': item.title}
+            })
+        .then(data =>{
+            console.log(data + ' inserted');
+            console.log(item.place + ' inserted');
+            if(iterator < portData.length - 1){
+                iterator++;
+                cb(iterator, cb);
+                return;
+            } else {
                 process.exit(0);
-            };
+            }
         })
         .catch(err => {
-            console.error(err);
-            process.exit(1);
+            try{         
+                if(iterator < portData.length){
+                    console.error(err);
+                    iterator++;
+                    cb(iterator, cb);
+                    return;
+                } else {
+                    console.error(err);
+                    iterator++;
+                    cb(iterator, cb);
+                    return;
+                };
+            } catch(err2){
+                console.error(err2);
+                process.exit(1);
+            };
         });
-});
+};
 
-}); //end chain
+function dbChanger(){
+    db.PicData.remove({})
+    .then(() =>{
+        seeder(0, seeder);
+    });
+};
 
+dbChanger();
