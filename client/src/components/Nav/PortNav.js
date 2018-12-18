3209d1ecf47da8d67e40ec9a20f4ab4d3833a296
Cragady <pinkdotdreamer@gmail.com>
2018-12-17 19:16:55 -0700

    added accordion to dropdown button

--- client/src/components/Nav/PortNav.js
+++ client/src/components/Nav/PortNav.js
@@ -12,18 +12,14 @@ const butArr = ['Portfolio', 'No-Script', 'Basic',
     'Mongoose', 'ES6', 'React', 'Yarn', 'MERN', 'Axios',
 ];
 
-function btnShower(event){
-    const name = event.target.name,
-        target = event.target,
-        element = document.getElementById('show-hide');
+function accordionBtn(){
+    const element = document.getElementById('show-hide');
 
-    if(name === 'hidden'){
-        element.style.display = 'block';
-        target.setAttribute('name', 'shown');
-    } else if (name === 'shown'){
-        element.style.display = 'none';
-        target.setAttribute('name', 'hidden');
-    };
+    if(element.style.maxHeight){
+        element.style.maxHeight = null;
+    } else {
+        element.style.maxHeight = element.scrollHeight + 'px';
+    }
 };
 
 export const PortNav = props =>{
@@ -31,24 +27,27 @@ export const PortNav = props =>{
         <section className='port-head rounded'>
             <h1 className='text-white'>Projects</h1>
 
-            <div className='my-1'>
-                <ul className='nav nav-cust-port'>
-                    <li className='btn no-btn ml-auto mr-3' >Project type:</li>
-                    <li className='mr-auto mb-2' >
-                        <input className='form-control' placeholder={props.fType ? props.fType : 'All'}></input>
+            <div className='my-1 nav-cust-port'>
+                {/* <ul className='nav mb-5'>
+                    <li className='btn no-btn ml-auto mx-auto' >Project type:</li>
+                    <li className='mr-auto mx-auto mb-2 input-parent' >
+                        //<input className='btn-ports' placeholder={props.fType ? props.fType : 'All'}></input>
+                        <input className='form-control btn-ports'></input>
+                        <input className='form-control autocomplete btn-ports' disabled value='test' />
                     </li>
-                    <li>
-                        <button className='btn' name='hidden' onClick={btnShower}>Click Me!</button>
-                    </li>
-                    <div className='types-holder'>
-                        <div className='btn-in-hiding' id='show-hide'>
-                            <button className='btn btn-port m-1' name='' defaultValue onClick={props.onClick} value='' >All</button>
-                            {butArr.map(types =>{
-                                return(<button className='btn btn-port m-1' name={types} key={types + '-skills'} onClick={props.onClick}>{types}</button>)
-                            })}
-                        </div>
+                </ul> */}
+                <div>Filtered By: </div>
+                <div className='btn-acc-holder mx-auto'>
+                    <button className='btn dropdown-toggle btn-accordion' onClick={accordionBtn}>{props.fType ? props.fType : 'All'}</button>
+                </div>
+                <div className='types-holder'>
+                    <div className='btn-in-hiding' id='show-hide'>
+                        <button className='btn btn-port m-1' name='' defaultValue onClick={(event) => {props.onClick(event); accordionBtn(event)}} value='' >All</button>
+                        {butArr.map(types =>{
+                            return(<button className='btn btn-port m-1' name={types} key={types + '-skills'} onClick={(event) => {props.onClick(event); accordionBtn(event)}}>{types}</button>)
+                        })}
                     </div>
-                </ul>
+                </div>
             </div>
         </section>
     );
