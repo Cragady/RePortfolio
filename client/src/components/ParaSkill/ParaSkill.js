2e157dabd7e70e1ffa7fa09f78c7523f75bacc4e
Cragady <pinkdotdreamer@gmail.com>
2019-02-03 16:13:50 -0700

    added parallax effect with divs; will use this to drag down icons for skills

---
+++ client/src/components/ParaSkill/ParaSkill.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './ParaSkill.css';
+
+export class ParaSkill extends Component{
+
+    componentDidMount(){
+        const paraskillElements = document.getElementsByClassName('paraskill'),
+            paraskillQuantity = paraskillElements.length;
+            window.addEventListener('scroll', () => this.paraskillScroll(paraskillElements, paraskillQuantity));
+    }
+
+    paraskillScroll = (elem, quant) =>{
+        // const paraskillElements = document.getElementsByClassName('paraskill'),
+        //     paraskillQuantity = paraskillElements.length;
+
+
+        // window.onscroll(function(){
+            window.requestAnimationFrame(function(){
+                for(let i = 0; i < quant; i++){
+                    const currentElement = elem[i];
+                    console.log(currentElement);
+                    const scrolled = window.pageYOffset || document.documentElement.scrollTop;
+                    console.log(scrolled + " scrolled");
+                    currentElement.style.transform = `translate3d(0, ${scrolled * 1.6}px, 0)`;
+                    // currentElement.css({
+                    //     'transform': `translate3d(0, ${scrolled * 1.6}px, 0)`
+                    // });
+                };
+            });
+        // });
+
+
+    };
+
+    render(){
+        return(
+            <section className='paraskill-container rounded'>
+                <div className='paraskill align-items-end'>
+                    <p className='para-p'>Hello</p>
+                </div>
+            </section>
+        );
+    };
+};
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { ParaSkill } from '../components/ParaSkill';
 import './Pages.css';
 
 export class Home extends Component{
@@ -6,20 +7,22 @@ export class Home extends Component{
         return(
             <section className="container cont-cust">
 
-                    <div className="card-header">
-                        <h3 className="text-center border-top border-secondary text-white pt-3">Welcome! I'm Craig, a certified Full Stack Developer</h3>
+                <div className="card-header">
+                    <h3 className="text-center border-top border-secondary text-white pt-3">Welcome! I'm Craig Wright, a certified Full Stack Developer</h3>
+                </div>
+
+                <div className='row'>
+                    <div className='col'>
+                        <img className='rounded img-fluid img-me mb-1' src='/images/propic2.jpg' alt='Craig Wright' />
                     </div>
 
-                    <div className='row'>
-                        <div className='col'>
-                            <img className='rounded img-fluid img-me mb-1' src='/images/propic2.jpg' alt='Craig Wright' />
-                        </div>
-
-                        <div className='col quick-intro text-white align-self-center'>
-                            <p>As a developer, my goal is to ever expand my skill set, enabling me to be versatile in any work environment.</p>
-                        </div>
+                    <div className='col quick-intro text-white align-self-center'>
+                        <p>As a developer, my goal is to ever expand my skill set, enabling me to be versatile in any work environment.</p>
                     </div>
+                </div>
                     
+                <ParaSkill />
+
             </section>
         );
     };
