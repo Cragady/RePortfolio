e3401eb9d8e47783781b6beaf9debb8089b76707
Cragady <pinkdotdreamer@gmail.com>
2019-02-06 11:27:20 -0700

    added new way of stopping parallax; needs polishing

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,65 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
 
-export const Nav = props =>{
-    return(
-        <div className='da-navs'>
-            <ul className='nav nav-cust'>
-                <li className='nav-item li-item-cust'>
-                    <Link
-                        to='/'
-                        className={
-                            window.location.pathname === '/' ? 'nav-link active' : 'nav-link inactive'
-                        }
-                        id={
-                            window.location.pathname === '/' ? 'active-cust' : ''
-                        }
-                    >
-                        Home
-                    </Link>
-                </li>
-                <li className='nav-item li-item-cust'>
-                    <Link
-                        to='/portfolio'
-                        className={window.location.pathname === '/portfolio' ? 'nav-link active' : 'nav-link inactive'}
-                        id={window.location.pathname === '/portfolio' ? 'active-cust' : ''}
-                    >
-                        Portfolio
-                    </Link>
-                </li>
-                <li className='nav-item li-item-cust'>
-                    <Link 
-                        to='/about'
-                        className={window.location.pathname === '/about' ? 'nav-link active' : 'nav-link inactive'}
-                        id={window.location.pathname === '/about' ? 'active-cust' : ''}
-                    >
-                        About Me
-                    </Link>
-                </li>
-                {/* <li className='ml-auto mr-1 d-flex align-items-center'>
-                    {props.login !== '' ? <DropAc logout={props.logout} login={props.login} /> : (
-                        <div>
-                            <A hrefless='true'
-                                data-toggle='modal'
-                                data-target='#modalLogIn'
-                            classext='d-inline'>
-                                Login
-                            </A> <span className='or'>or</span> <A 
-                                hrefless='true'
-                                data-toggle='modal'
-                                data-target='#modalSignUp'
-                            classext='d-inline'>
-                                Sign Up
-                            </A>
-                            <div className='mod-dump'>
-                                <SignModal modId='modalLogIn' onChange={props.onChange} onClick={props.submitLo} />
-                                <SignModal modId='modalSignUp' onChange={props.onChange} onClick={props.submitSi} />
+export class Nav extends Component{
+
+    render(){
+        return(
+            <div className='da-navs'>
+                <ul className='nav nav-cust'>
+                    <li className='nav-item li-item-cust'>
+                        <Link
+                            to='/'
+                            className={
+                                window.location.pathname === '/' ? 'nav-link active' : 'nav-link inactive'
+                            }
+                            id={
+                                window.location.pathname === '/' ? 'active-cust' : ''
+                            }
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item li-item-cust'>
+                        <Link
+                            to='/portfolio'
+                            className={window.location.pathname === '/portfolio' ? 'nav-link active' : 'nav-link inactive'}
+                            id={window.location.pathname === '/portfolio' ? 'active-cust' : ''}
+                        >
+                            Portfolio
+                        </Link>
+                    </li>
+                    <li className='nav-item li-item-cust'>
+                        <Link 
+                            to='/about'
+                            className={window.location.pathname === '/about' ? 'nav-link active' : 'nav-link inactive'}
+                            id={window.location.pathname === '/about' ? 'active-cust' : ''}
+                        >
+                            About Me
+                        </Link>
+                    </li>
+                    {/* <li className='ml-auto mr-1 d-flex align-items-center'>
+                        {props.login !== '' ? <DropAc logout={props.logout} login={props.login} /> : (
+                            <div>
+                                <A hrefless='true'
+                                    data-toggle='modal'
+                                    data-target='#modalLogIn'
+                                classext='d-inline'>
+                                    Login
+                                </A> <span className='or'>or</span> <A 
+                                    hrefless='true'
+                                    data-toggle='modal'
+                                    data-target='#modalSignUp'
+                                classext='d-inline'>
+                                    Sign Up
+                                </A>
+                                <div className='mod-dump'>
+                                    <SignModal modId='modalLogIn' onChange={props.onChange} onClick={props.submitLo} />
+                                    <SignModal modId='modalSignUp' onChange={props.onChange} onClick={props.submitSi} />
+                                </div>
                             </div>
-                        </div>
-                    )}
-                </li> */}
-            </ul>
-        </div>
-    );
-};
+                        )}
+                    </li> */}
+                </ul>
+            </div>
+        );
+    };
+    }
--- client/src/components/ParaSkill/ParaSkill.js
+++ client/src/components/ParaSkill/ParaSkill.js
@@ -3,20 +3,65 @@ import './ParaSkill.css';
 
 export class ParaSkill extends Component{
 
+    //bottom of parralax element at scroll position 0 === 349.20001220703125
+    //position to reach with current layout 918.85498046875
+    // 918.85498046875 - 349.20001220703125
+    //569.6549682617188
+    // actual bottom starting point 418.375
+
     componentDidMount(){
+        const passThis = this.offset;
+        const startScroll = window.pageYOffset || document.documentElement.scrollTop;
+
         window.addEventListener('scroll', this.paraskillScroll);
+
+        setTimeout(function(){
+            const movSel = document.getElementsByClassName('paraskill'),    
+                movePic = document.querySelector('.paraskill');
+            let picPos;
+
+            if(startScroll === 0){
+                picPos = passThis(movePic, 'bottom');
+            } else {
+                picPos = passThis(movePic, 'bottom') - (startScroll * 1.6);
+                console.log(picPos);
+            };
+            movSel[0].dataset.origpos = picPos;
+        });
     };
 
     componentWillUnmount(){
         window.removeEventListener('scroll', this.paraskillScroll);
     };
 
-    offset = (el) =>{
+    offset = (el, pos) =>{
         const elRect = el.getBoundingClientRect(),
             bodRect = document.body.getBoundingClientRect(),
-            top = elRect.bottom - bodRect.top;
-            
-        return top;
+            dir = elRect[pos] - bodRect.top;
+        return dir;
+    };
+
+    topInit = () =>{
+        const sk = document.querySelector('.paraskill');
+        const sb = this.offset(sk, 'bottom');
+        console.log(sb, 'ssss bbbb');
+    };
+
+    calculateRScroll = (scrolled, reverser) =>{
+        const sePlace = document.querySelector('.paraskill'),
+            seClassPlace = document.getElementsByClassName('paraskill')[0],
+            sTop = this.offset(sePlace, 'bottom'),
+            pBottom = document.querySelector('.paraskill-container'),
+            pottom = this.offset(pBottom, 'bottom');
+        let oPos = seClassPlace.dataset.origpos;
+        if(reverser !== undefined){
+            console.log('reverse! reverse!');
+        };
+        if(oPos !== undefined){
+            oPos = parseFloat(oPos);
+            const right = (((scrolled * 1.6) + oPos) - pottom) * -1;
+            return right;
+        };
     };
 
     paraskillScroll = () =>{
@@ -24,25 +69,43 @@ export class ParaSkill extends Component{
             sePlace = document.querySelector('.paraskill'),
             paraskillQuantity = paraskillElements.length,
             pElem = document.querySelector('.paraskill-container'),
-            pBottom = this.offset(pElem),
-            cBottom = this.offset(sePlace);
-            
+            pBottom = this.offset(pElem, 'bottom'),
+            cBottom = this.offset(sePlace, 'bottom'),
+            pTop = this.offset(pElem, 'top'),
+            sTop = this.offset(sePlace, 'bottom'),
+            sCalc = this.calculateRScroll;
+
         window.requestAnimationFrame(function(){
             for(let i = 0; i < paraskillQuantity; i++){
                 const currentElement = paraskillElements[i],
                     scrolled = window.pageYOffset || document.documentElement.scrollTop,
                     scrollSave = currentElement.dataset.scrollSave;
+                let posInherit;
 
-                if (scrolled < scrollSave){
+                
+                // if (scrolled < scrollSave){
+                if (sCalc(scrolled) > 0){
                     currentElement.style.transform = `translate3d(0, ${scrolled * 1.6}px, 0)`;
-                } else if (pBottom === cBottom || pBottom < cBottom){
-                    if(scrollSave === undefined){
-                        currentElement.dataset.scrollSave = scrolled;
-                    };
-                    return;
-                } else {
-                    currentElement.style.transform = `translate3d(0, ${scrolled * 1.6}px, 0)`;
-                };
+                    currentElement.style.bottom = 100 + '%';
+                    // currentElement.dataset.scrollSave = undefined;
+                // } else if (pBottom === cBottom || pBottom < cBottom){
+                } else {//if (sCalc(scrolled) < 0){
+                    currentElement.style.bottom = pBottom;
+                    posInherit = currentElement.style.bottom;
+                    console.log(sCalc(scrolled, 'true'));
+                    
+                    // if(scrollSave === undefined || scrollSave === 'undefined'){
+                    //     currentElement.dataset.scrollSave = scrolled;
+                    // };
+                    // return;
+                };// else {
+                //     currentElement.style.transform = `translate3d(0, ${scrolled * 1.6}px, 0)`;
+                // };
+                console.log(sCalc(scrolled));
+                console.log(posInherit, 'inheritor');
+                if((sCalc(scrolled) < 0) && (currentElement.style.bottom !== pBottom)){
+                    currentElement.style.bottom = pBottom;
+                }
             };
         });
     };
