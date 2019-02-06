b7886495c809389a70eae54ac71331813dba3638
Cragady <pinkdotdreamer@gmail.com>
2019-02-06 15:21:11 -0700

    fixed parallax stop point

--- client/src/components/ParaSkill/ParaSkill.js
+++ client/src/components/ParaSkill/ParaSkill.js
@@ -3,15 +3,8 @@ import './ParaSkill.css';
 
 export class ParaSkill extends Component{
 
-    //bottom of parralax element at scroll position 0 === 349.20001220703125
-    //position to reach with current layout 918.85498046875
-    // 918.85498046875 - 349.20001220703125
-    //569.6549682617188
-    // actual bottom starting point 418.375
-
     componentDidMount(){
         const passThis = this.offset;
-        const startScroll = window.pageYOffset || document.documentElement.scrollTop;
 
         window.addEventListener('scroll', this.paraskillScroll);
 
@@ -20,12 +13,7 @@ export class ParaSkill extends Component{
                 movePic = document.querySelector('.paraskill');
             let picPos;
 
-            if(startScroll === 0){
-                picPos = passThis(movePic, 'bottom');
-            } else {
-                picPos = passThis(movePic, 'bottom') - (startScroll * 1.6);
-                console.log(picPos);
-            };
+            picPos = passThis(movePic, 'bottom');
             movSel[0].dataset.origpos = picPos;
         });
     };
@@ -33,6 +21,13 @@ export class ParaSkill extends Component{
     componentWillUnmount(){
         window.removeEventListener('scroll', this.paraskillScroll);
     };
+
+    positionListener = (s, sTar, cBot, pBot, cElem) =>{
+        if((s > sTar) && (cBot < pBot) || (cBot > pBot)){
+            cElem.style.transform = `translate3d(0, ${sTar * 1.6}px, 0)`;
+        };
+    }
+
 
     offset = (el, pos) =>{
         const elRect = el.getBoundingClientRect(),
@@ -48,20 +43,26 @@ export class ParaSkill extends Component{
     };
 
     calculateRScroll = (scrolled, reverser) =>{
-        const sePlace = document.querySelector('.paraskill'),
-            seClassPlace = document.getElementsByClassName('paraskill')[0],
-            sTop = this.offset(sePlace, 'bottom'),
+        const seClassPlace = document.getElementsByClassName('paraskill')[0],
             pBottom = document.querySelector('.paraskill-container'),
             pottom = this.offset(pBottom, 'bottom');
-        let oPos = seClassPlace.dataset.origpos;
-        if(reverser !== undefined){
+        let oPos = seClassPlace.dataset.origpos,
+        returner;
+
+        if((reverser === 'true') && (oPos !== undefined)){
             console.log('reverse! reverse!');
-        };
-        if(oPos !== undefined){
             oPos = parseFloat(oPos);
-            const right = (((scrolled * 1.6) + oPos) - pottom) * -1;
-            return right;
+            const reverse = (pottom - oPos) / 1.6;
+            console.log('reverse', reverse, "then scroll", scrolled);
+            returner = reverse;
         };
+        if((oPos !== undefined) && (reverser !== 'true')){
+            oPos = parseFloat(oPos);
+            
+            const right = (((scrolled * 1.6) + oPos) - pottom) * -1;
+            returner = right;
+        };
+        return returner;
     };
 
     paraskillScroll = () =>{
@@ -71,41 +72,23 @@ export class ParaSkill extends Component{
             pElem = document.querySelector('.paraskill-container'),
             pBottom = this.offset(pElem, 'bottom'),
             cBottom = this.offset(sePlace, 'bottom'),
-            pTop = this.offset(pElem, 'top'),
-            sTop = this.offset(sePlace, 'bottom'),
-            sCalc = this.calculateRScroll;
+            sCalc = this.calculateRScroll,
+            thisPass = this;
 
         window.requestAnimationFrame(function(){
             for(let i = 0; i < paraskillQuantity; i++){
                 const currentElement = paraskillElements[i],
                     scrolled = window.pageYOffset || document.documentElement.scrollTop,
-                    scrollSave = currentElement.dataset.scrollSave;
-                let posInherit;
+                    scrollPoint = sCalc(scrolled),
+                    scrollComparator = sCalc(scrolled, 'true');
 
-                
-                // if (scrolled < scrollSave){
-                if (sCalc(scrolled) > 0){
+                if (scrollPoint > 0){
                     currentElement.style.transform = `translate3d(0, ${scrolled * 1.6}px, 0)`;
                     currentElement.style.bottom = 100 + '%';
-                    // currentElement.dataset.scrollSave = undefined;
-                // } else if (pBottom === cBottom || pBottom < cBottom){
-                } else {//if (sCalc(scrolled) < 0){
+                } else {
                     currentElement.style.bottom = pBottom;
-                    posInherit = currentElement.style.bottom;
-                    console.log(sCalc(scrolled, 'true'));
-                    
-                    // if(scrollSave === undefined || scrollSave === 'undefined'){
-                    //     currentElement.dataset.scrollSave = scrolled;
-                    // };
-                    // return;
-                };// else {
-                //     currentElement.style.transform = `translate3d(0, ${scrolled * 1.6}px, 0)`;
-                // };
-                console.log(sCalc(scrolled));
-                console.log(posInherit, 'inheritor');
-                if((sCalc(scrolled) < 0) && (currentElement.style.bottom !== pBottom)){
-                    currentElement.style.bottom = pBottom;
-                }
+                    thisPass.positionListener(scrolled, scrollComparator, cBottom, pBottom, currentElement);
+                };
             };
         });
     };
@@ -114,7 +97,10 @@ export class ParaSkill extends Component{
         return(
             <section className='paraskill-container rounded'>
                 <div className='paraskill align-items-end'>
-                    <p className='para-p'>Hello</p>
+                    <section className='para-elem'>
+                        <button>Push me</button>
+                        <p>Hello</p>
+                    </section>
                 </div>
             </section>
         );
