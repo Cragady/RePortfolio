2e85b01a24ea01dd2dd7ea3ef02be23a1db66bb2
Cragady <pinkdotdreamer@gmail.com>
2019-02-10 00:09:23 -0700

    changing protocols

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+if(window.location.hostname !== 'localhost'){
+    if(window.location.protocol !== 'https:'){
+        window.location.protocol = 'https:';
+    };
+};
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
