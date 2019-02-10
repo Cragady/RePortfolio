34d6bd84a14d8a92873ece012cdb2794abbaffae
Cragady <pinkdotdreamer@gmail.com>
2019-02-10 13:16:55 -0700

    switch for url

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-if(window.location.hostname !== 'localhost'){
-    if(window.location.protocol !== 'https:'){
-        window.location.protocol = 'https:';
+if(window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1'){
+    switch (true){
+        case window.location.hostname === 'evening-beach-49814.herokuapp.com':
+            window.location.replace('https://www.craigwright2048.com');
+            break;
+        case window.location.protocol !== 'https:':
+            window.location.protocol = 'https:'
+            break;
+        default:
+            console.log('err');
     };
 };
 
