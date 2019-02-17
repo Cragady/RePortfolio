464a2ed9cb77c62e535f8a662de9206302deda80
Cragady <pinkdotdreamer@gmail.com>
2019-02-16 21:23:21 -0700

    added badge to page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -39,6 +39,8 @@ export class Home extends Component{
                 
                 <ParaSkill />
 
+                <div className='badge mt-5' ><a href='https://www.youracclaim.com/badges/d41a41a4-1de2-48d4-8d46-545d4ff49c9b/public_url' target='_blank' rel='noopener noreferrer'><img className='img-fluid badge-img' src='/images/badge/badge-coding-bootcamp-trilogy.png' alt='certificate badge' /></a></div>
+
             </section>
         );
     };
